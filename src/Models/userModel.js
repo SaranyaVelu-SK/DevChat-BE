80fcce0e518be0055649e1cc78c8916e3f697d55
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-08-05 15:10:53 +0530

    created schema,model and two apis to fetch user data  and signup

---
+++ src/Models/userModel.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName:{            //firstName:String ===> firstName:{type:String}
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String
+        },
+        email:{
+            type:String,
+            required:true,
+            trim:true,      //trims the extra whitespace ---> calls .trim() with the email value we give
+            unique:true,     //unique is NOT a validator
+            validate:{
+                validator : function(value){            //validator function
+                    return validator.isEmail(value)         //validator package by npm
+                },
+                message : function(props){                  //props contains path(field name i.e email here) and value(value of the email given)
+                    return `${props.value} is not a valid email address`
+                }
+            }
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        age:{
+            type:Number,
+            min:[18,"user must be above 18 years to sign up"],              //customized error message for this validation - can be used for all validations like min, max, required
+            max:[60, "user must be below 60 yrs, but you are {VALUE}"]              //customized error msg with the input value
+        },
+        gender:{
+            type:String,
+            enum:{
+                    values:["male","female","others"],              //custom error mesaage for VALIDATIONS
+                    message:"{VALUE} is not a valid gender"
+            }
+        },
+        profilePic:{
+            type:String,
+            validate:{
+                validator: function(value){
+                    return validator.isURL(value)
+                },
+                message:function(props){
+                    return `not a valid url`
+                }
+            }
+        },
+        skills:{
+            type:[String],        //array of strings
+        },
+        description:{
+            type:String,
+            default:function(){                         
+                return `Hi , I'm ${this.firstName}`
+            }
+        }
+    },
+    {
+        timestamps: true    //date and time that represents when the document is created and updated --->changes during POST,PUT and PATCH
+    },
+    {
+        collection : 'user'   //explicitly adding the collection name from mongoDB to this schema
+    });
+
+   const UserModel = mongoose.model("User",userSchema);
+
+   module.exports = {UserModel}
--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const app = express();
 const { connectToDB } = require('./Config/database')
+const{UserModel} =require('./Models/userModel')
 
+app.use(express.json())
+app.post('/signUp', async(req,res)=>{
+    const signedUpUser = new UserModel(req.body);
+    try{
+        await signedUpUser.save();
+        res.send("signed up successfully")
+    }catch(err){
+        res.status(400).send(err);
+        console.log(err)
+    }
+})
 
+app.get('/feed',async (req,res)=>{
+    try{
+        const usersFeed = await UserModel.find({});
+        res.send(usersFeed)
+    }catch(err){
+        res.status(400).send(err);
+        console.log(err)
+    }
+})
 
-
+app.get('/getUser',async (req,res)=>{
+    const email = req.body.email;
+    try{
+        const userData = await UserModel.findOne({email:email});
+        res.send(userData)
+    }catch(err){
+        res.status(400).send(err);
+        console.log(err)
+    }
+})
 
 connectToDB().then(() => {
     console.log("database connected successfully");
